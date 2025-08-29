f257b2f223b91ab56d0b8463ab453eabd18770ed
bhaveshrout <888akabhavesh@gmail.com>
2025-08-29 16:09:13 +0530

    commit 1

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -10,16 +10,8 @@ const AboutUs = () => {
         <Spline scene="https://prod.spline.design/60yFZPeBQBqF9vqM/scene.splinecode" />
       </div>
       <div className="about-us-content">
-        <h1>About Us</h1>
-        <p>
-          Welcome to Cyfernode! We are dedicated to bridging the gap between technology and creativity. Our mission is to empower individuals through innovative learning experiences that blend online and offline formats.
-        </p>
-        <p>
-          At Cyfernode, we believe in the power of collaboration and community. Whether you're a tech enthusiast, a creative thinker, or someone looking to enhance your skills, we provide a platform for you to grow and thrive.
-        </p>
-        <p>
-          Join us on this exciting journey as we craft tomorrow's solutions today!
-        </p>
+        <h1>Turning ideas into creations that <span className="fdsddgdf">define the future.</span></h1>
+
       </div>
     </div>
   );
