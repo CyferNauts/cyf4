3d1c97798c37dfd3cb74d05221323cfd07f862a9
bhaveshrout <888akabhavesh@gmail.com>
2025-08-29 23:38:58 +0530

    commit 2

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,17 +1,34 @@
 import React from "react";
 
 import Spline from "@splinetool/react-spline";
-import "./AboutUs.css"; // Assuming a CSS file will be created for styling
+import "./AboutUs.css"; 
+
+
+import dayum from "./PinDown.io_@eplerkirill_1756479385.mp4";
+import tagline from "./Tagline.svg";
+import iconimg from "./icon1.png"; // Assuming a CSS file will be created for styling
 
 const AboutUs = () => {
   return (
     <div className="about-us">
-       <div className="spline-overlayaboutus">
-        <Spline scene="https://prod.spline.design/60yFZPeBQBqF9vqM/scene.splinecode" />
+       <div className="spline-overlayaboutus aboutus_wala_hai_ye">
+      <video 
+        autoPlay 
+        loop 
+        muted 
+        className="aboutus-vid"
+      >
+        <source src={dayum} type="video/mp4" />
+      </video>
       </div>
       <div className="about-us-content">
-        <h1>Turning ideas into creations that <span className="fdsddgdf">define the future.</span></h1>
+        <img className="tagline" src={tagline} alt="About Us" />
 
+        <div className="des">
+          <img className="icon" src={iconimg} alt="About Us" />
+          <p className="about-us-text">We are a team of passionate individuals who are dedicated to making a positive impact on the world. Our mission is to create innovative solutions that address the challenges faced by society, and to use technology to improve people's lives.</p>
+        </div>
+      
       </div>
     </div>
   );
